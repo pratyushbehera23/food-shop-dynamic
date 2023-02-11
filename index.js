64963152d5b67e6be90ae158f4c8aa74b60685ba
pratyushbehera23 <pratyushbehera.mail@gmail.com>
2023-02-11 23:12:07 +0530

    initial commit (basic set)

---
+++ index.js
@@ -0,0 +1,36 @@
+// Imports:
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+// 
+const data = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+// Server
+const server = http.createServer((req, res) => {
+    // res.end("Hello from the server");
+    // console.log(req.url);
+    const pathName = req.url;
+
+    if(pathName === '/' || pathName === '/overview'){
+        res.end("Overview page");
+    } else if(pathName === '/products'){
+        res.end("Products page");
+    } else if(pathName === '/api'){
+        res.writeHead(200, {
+            'Content-type': 'application/json'
+        })
+        res.end(data);
+    } else{
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found!</h1>')
+    }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to request on port 8000');
+});
